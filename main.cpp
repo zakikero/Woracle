b7da44a87012bdd373faee689e829696c05e641d
zakikero <zakikerouani@gmail.com>
2026-01-14 16:16:22 -0500

    Refactor code input validation and improve main loop structure in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,33 +10,34 @@
 
 const std::string DEFAULT_DICTIONARY_PATH = "./fiveLetterWords/words.txt";
 
-bool isCodeInputValid(const std::string& codeInput)
-{
+bool isCodeInputValid(const std::string &codeInput) {
     if (codeInput.length() != 5) return false;
 
-    return std::ranges::all_of(codeInput, [](const char c)
-    {
+    return std::ranges::all_of(codeInput, [](const char c) {
         return c == 'g' || c == 'y' || c == 'b';
     });
 }
 
 
-int main(int argc, char* argv[])
-{
+int main() {
     std::vector<std::string> dictionary = timedReadDictionary(DEFAULT_DICTIONARY_PATH);
 
-    std::cout << "input the word FLUTE to start and give me the color of each letters in sequence \nfor example : ygbyb " <<
-        std::endl;
+    std::cout <<
+            "input the word FLUTE to start and give me the color of each letters in sequence \nfor example : ygbyb " <<
+            std::endl;
 
     std::string codeInput;
-    while (true)
-    {
+    while (true) {
         std::cout << "Enter code :\t";
         std::cin >> codeInput;
         if (!isCodeInputValid(codeInput)) continue;
 
+        // add the code to the previous word
 
+
+        // figure out the next guess
 
     }
+
     return 0;
 }
