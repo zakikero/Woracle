c372b194f7f233e2a4a006cefd4de72403a2fb93
zakikero <zakikerouani@gmail.com>
2026-01-14 16:16:06 -0500

    Implement code input handling and black letter filtering in WordleSolver

--- include/WordleSolver.h
+++ include/WordleSolver.h
@@ -6,20 +6,32 @@
 #define WORACLE_WORDLESOLVER_H
 #include <string>
 #include <vector>
+#include <unordered_set>
 
+const std::string DEFAULT_STARTING_WORD = "FLUTE";
 
-class WordleSolver
-{
+class WordleSolver {
 public:
-    explicit WordleSolver(const std::vector<std::string>& dictionary): possibleWords(dictionary) {}
+    explicit WordleSolver(const std::vector<std::string> &dictionary) : possibleWords(dictionary) {
+        guesses.reserve(5);
+    }
 
-    void addCodeInput(const std::string& codeInput);
+    void addCodeInput(const std::string &codeInput);
+
+    void addGuess(const std::string &wordGuessed, const std::string &codeInput);
+
     void getNextWordGuess();
 
 private:
     void filterPossibleWords();
 
+    void filterOutBlackLetters();
+    void addBlackLetter(const char& blackLetter);
+
+    std::unordered_set<char> blackLetters;
+
     std::vector<std::string> possibleWords;
+    std::vector<std::pair<std::string, std::string> > guesses; // pair of <word guessed, code input>
     std::vector<std::string> codeInputs;
 };
 
--- src/WordleSolver.cpp
+++ src/WordleSolver.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "WordleSolver.h"
+
+#include <algorithm>
+#include <stdexcept>
+
+
+void WordleSolver::addCodeInput(const std::string &codeInput) {
+    this->codeInputs.push_back(codeInput);
+}
+
+void WordleSolver::addGuess(const std::string &wordGuessed, const std::string &codeInput) {
+    guesses.emplace_back(wordGuessed, codeInput);
+}
+
+void WordleSolver::filterOutBlackLetters() {
+    const auto it = std::ranges::remove_if(possibleWords,
+                                           [this](const std::string &word) {
+                                               return std::ranges::any_of(word, [this](const char &letter) {
+                                                   return blackLetters.contains(letter);
+                                               });
+                                           }).begin();
+    possibleWords.erase(it, possibleWords.end());
+}
+
+void WordleSolver::addBlackLetter(const char &blackLetter) {
+    if (blackLetters.contains(blackLetter))
+        throw std::runtime_error("Black Letter Already exists, algorithm error");
+
+    blackLetters.insert(blackLetter);
+}
+
+void WordleSolver::getNextWordGuess() {
+}
+
+void WordleSolver::filterPossibleWords() {
+}
