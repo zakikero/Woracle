c2376c4eea6ce404c935828113ffd9678b13ebd5
zakikero <zakikerouani@gmail.com>
2026-01-22 14:05:03 -0500

    Implement guess processing and filtering logic in WordleSolver

--- include/WordleSolver.h
+++ include/WordleSolver.h
@@ -4,35 +4,64 @@
 
 #ifndef WORACLE_WORDLESOLVER_H
 #define WORACLE_WORDLESOLVER_H
+#include <queue>
 #include <string>
 #include <vector>
 #include <unordered_set>
 
+#include "PositionalExclusionMap.h"
+#include "SolutionMatcher.h"
+
+constexpr std::size_t MAX_GUESS_AMOUNT = 6;
 const std::string DEFAULT_STARTING_WORD = "FLUTE";
+
 
 class WordleSolver {
 public:
-    explicit WordleSolver(const std::vector<std::string> &dictionary) : possibleWords(dictionary) {
-        guesses.reserve(5);
+    explicit WordleSolver(const std::vector<std::string> &dictionary)
+        : greenMatcher(SolutionMatcher()),
+          possibleWords(dictionary) {
     }
 
-    void addCodeInput(const std::string &codeInput);
+    void processGuessResults(const std::string &wordGuessed, const std::string &codeInput);
 
-    void addGuess(const std::string &wordGuessed, const std::string &codeInput);
+    std::string getNextWordGuess();
 
-    void getNextWordGuess();
+    void printGuessesStack();
+
+    bool isSolutionFound() const {
+        return greenMatcher.isComplete();
+    }
+
+    std::string getSolutionPattern() {
+        return greenMatcher.getSolutionPattern();
+    }
 
 private:
+    void addGuess(const std::string &wordGuessed, const std::string &codeInput);
+
     void filterPossibleWords();
 
     void filterOutBlackLetters();
-    void addBlackLetter(const char& blackLetter);
+
+    void filterOutGreenLetters();
+
+    void filterOutYellowLetters();
+
+
+    void addBlackLetter(const char &blackLetter);
+
+    void addYellowLetter(const char &yellowLetter, const size_t &position);
+
+    void addGreenLetter(const char &greenLetter, const size_t &position);
 
     std::unordered_set<char> blackLetters;
+    std::unordered_set<char> yellowLetters;
+    PositionalExclusionMap yellowExclusionsMap;
+    SolutionMatcher greenMatcher;
 
     std::vector<std::string> possibleWords;
-    std::vector<std::pair<std::string, std::string> > guesses; // pair of <word guessed, code input>
-    std::vector<std::string> codeInputs;
+    std::queue<std::pair<std::string, std::string> > guesses; // pair of <word guessed, code input>
 };
 
 
--- src/WordleSolver.cpp
+++ src/WordleSolver.cpp
@@ -5,36 +5,97 @@
 #include "WordleSolver.h"
 
 #include <algorithm>
+#include <iostream>
 #include <stdexcept>
 
 
-void WordleSolver::addCodeInput(const std::string &codeInput) {
-    this->codeInputs.push_back(codeInput);
+void WordleSolver::processGuessResults(const std::string &wordGuessed, const std::string &codeInput) {
+    addGuess(wordGuessed, codeInput);
+    for (size_t i = 0; i < codeInput.length(); i++) {
+        char letter = wordGuessed[i];
+
+        if (const char codeChar = codeInput[i]; codeChar == 'b') {
+            addBlackLetter(letter);
+        } else if (codeChar == 'y') {
+            addYellowLetter(letter, i);
+        } else if (codeChar == 'g') {
+            addGreenLetter(letter, i);
+        } else {
+            throw std::invalid_argument("Invalid code character: " + std::string(1, codeChar));
+        }
+    }
+
+    filterPossibleWords();
+}
+
+std::string WordleSolver::getNextWordGuess() {
+    printGuessesStack();
+
+    const std::string nextGuess = *possibleWords.begin();
+    std::cout << "Possible words remaining: " << possibleWords.size() << std::endl;
+    std::cout << "next guess: " << nextGuess << std::endl;
+
+    return possibleWords.front();
+}
+
+void WordleSolver::printGuessesStack() {
+    std::queue<std::pair<std::string, std::string> > tempGuesses = guesses;
+    std::cout << "Guesses made so far:" << std::endl;
+    while (!tempGuesses.empty()) {
+        const auto &[word, code] = tempGuesses.front();
+        std::cout << "Word: " << word << ", Code: " << code << std::endl;
+        tempGuesses.pop();
+    }
 }
 
 void WordleSolver::addGuess(const std::string &wordGuessed, const std::string &codeInput) {
-    guesses.emplace_back(wordGuessed, codeInput);
-}
-
-void WordleSolver::filterOutBlackLetters() {
-    const auto it = std::ranges::remove_if(possibleWords,
-                                           [this](const std::string &word) {
-                                               return std::ranges::any_of(word, [this](const char &letter) {
-                                                   return blackLetters.contains(letter);
-                                               });
-                                           }).begin();
-    possibleWords.erase(it, possibleWords.end());
+    guesses.emplace(wordGuessed, codeInput);
 }
 
 void WordleSolver::addBlackLetter(const char &blackLetter) {
-    if (blackLetters.contains(blackLetter))
-        throw std::runtime_error("Black Letter Already exists, algorithm error");
-
     blackLetters.insert(blackLetter);
 }
 
-void WordleSolver::getNextWordGuess() {
+void WordleSolver::addYellowLetter(const char &yellowLetter, const size_t &position) {
+    yellowExclusionsMap.addExclusion(yellowLetter, position);
+    yellowLetters.insert(yellowLetter);
 }
 
+void WordleSolver::addGreenLetter(const char &greenLetter, const size_t &position) {
+    greenMatcher.addLetterAtPosition(greenLetter, position);
+}
+
+
 void WordleSolver::filterPossibleWords() {
+    filterOutBlackLetters();
+    filterOutYellowLetters();
+    filterOutGreenLetters();
+}
+
+void WordleSolver::filterOutBlackLetters() {
+    std::erase_if(possibleWords,
+                  [this](const std::string &word) {
+                      return std::ranges::any_of(word, [this](const char &letter) {
+                          return blackLetters.contains(letter);
+                      });
+                  });
+}
+
+void WordleSolver::filterOutYellowLetters() {
+    std::erase_if(possibleWords,
+                  [this](const std::string &word) {
+                      return yellowExclusionsMap.isWordExcluded(word);
+                  });
+    std::erase_if(possibleWords, [this](const std::string &word) {
+        return std::ranges::any_of(word, [this](const char &letter) {
+                return !yellowLetters.contains(letter);
+            });
+    });
+}
+
+void WordleSolver::filterOutGreenLetters() {
+    std::erase_if(possibleWords,
+                  [this](const std::string &word) {
+                      return !greenMatcher.isWordMatching(word);
+                  });
 }
