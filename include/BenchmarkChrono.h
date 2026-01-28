f8af4d1384ea35ad359c12c0404a86090a56cfe5
zakikero <zakikerouani@gmail.com>
2026-01-28 13:01:23 -0500

    Add displayElapsedNanoseconds method to BenchmarkChrono for outputting elapsed time

--- include/BenchmarkChrono.h
+++ include/BenchmarkChrono.h
@@ -5,6 +5,7 @@
 #ifndef WORACLE_BENCHMARKCHRONO_H
 #define WORACLE_BENCHMARKCHRONO_H
 #include <chrono>
+#include <iostream>
 
 
 class BenchmarkChrono {
@@ -14,6 +15,12 @@ public:
     void stop();
 
     [[nodiscard]] long long getElapsedNanoseconds() const;
+
+    void displayElapsedNanoseconds() const {
+        std::cout << std::endl;
+        std::cout << "Elapsed time: " << getElapsedNanoseconds() << " ns " << std::endl;
+        std::cout << std::endl;
+    }
 
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
